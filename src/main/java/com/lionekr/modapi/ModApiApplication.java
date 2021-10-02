ab34cf7723c46ac4a648fb95329e0a9f7f21cd40
lionekr <lionekr@gmail.com>
2021-10-02 11:44:37 +0200

    Initial commit

---
+++ src/main/java/com/lionekr/modapi/ModApiApplication.java
@@ -0,0 +1,13 @@
+package com.lionekr.modapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lionekr/modapi/ModApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionekr.modapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
